bd25a5cd67b1b863bfd542c69a85ee4c4ee034c5
Biagio Gennuso <gennuso.biagio@gmail.com>
2024-09-18 22:56:49 +0200

    Changed baseUrl

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api", // Assicurati che l'URL base sia corretto
+  baseURL: "https://backend-0q6p.onrender.com/api", // Assicurati che l'URL base sia corretto
   headers: {
     "Content-Type": "application/json",
   },
